124fd0210cdf92fba854b7c70d6b40d548560f56
Brad Fitzpatrick <brad@danga.com>
2023-02-02 14:27:47 -0800

    more
    Signed-off-by: Brad Fitzpatrick <brad@danga.com>

--- hi.go
+++ hi.go
@@ -11,6 +11,7 @@ func main() {
 	env, _ := json.MarshalIndent(os.Environ(), "", "  ")
 	envs := strings.ReplaceAll(string(env), "TOKEN", "NOT_A_THING")
 	fmt.Printf("Hello from Go. Env is: %s\n", envs)
+	fmt.Printf("Argv is %q\n", os.Args)
 
 	fmt.Printf("length of the not thing: %v\n", len(os.Getenv("GITHUB_TOKEN")))
 }
